e2cdfe734aca7d6ef7954ae4ae0fbb89ff313075
dizzy <84877191+drpratikk@users.noreply.github.com>
2022-02-01 22:28:10 +0530

    ..

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,15 +4,15 @@ import user from "../user.png";
 function ContactCard(props) {
   const { id, name, email } = props.contact;
   return (
-    <div className="ui item">
+    <div className="ui item"style={{margin:"3px",padding:"3px"}}>
       <img src={user} alt="user" className="ui avatar image" />
-      <div className=" ui content two column grid">
+      <div className=" ui content">
         <div className="ui large header">{name}</div>
-        <div className="ui small header">{email}</div>
+        <div className="ui small header">{email}</div><br />
         <i
-          className="trash alternate outline icon row"
+          className="trash alternate outline icon"
           onClick={() => props.clickHandle(id)}
-          style={{ color: "red"}}
+          style={{ color: "red",cursor:"pointer"}}
           />
           </div>
     </div>
